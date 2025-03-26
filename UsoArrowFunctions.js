cd8559030da042436746050d063d72c5655abfad
JeeJ <155782096+ggcoding11@users.noreply.github.com>
2025-03-26 14:19:15 -0300

    Add files via upload

---
+++ UsoArrowFunctions.js
@@ -0,0 +1,15 @@
+//Uso de arrow functions
+
+//Com parametros
+let funcaoSomar = (n1, n2) => {
+    return n1 + n2
+} 
+
+console.log(funcaoSomar(5,2))
+
+//Sem parametros
+let mostrarNome = () =>{
+    return "Gustavo de Almeida Gomes"
+}
+
+console.log(mostrarNome())
