b67980aaf3d5bfe91341e330482350cfe9921c0e
JeeJ <155782096+ggcoding11@users.noreply.github.com>
2025-03-31 13:29:41 -0300

    Add files via upload

---
+++ Lista - FrontEnd/JS/ex45.js
@@ -0,0 +1,9 @@
+let resultado = 0
+
+let i = 0
+
+while (i <= 100){
+    resultado += i++
+}
+
+console.log(resultado)
---
+++ Lista - FrontEnd/JS/ex49.js
@@ -0,0 +1,7 @@
+let nomes = ["Gustavo", "Ana", "Gabriel", "Pedro", "Ricardo"]
+
+nomes.push("Jeremias")
+
+for (let i = 0; i < nomes.length; i++){
+    console.log(nomes[i])
+}
